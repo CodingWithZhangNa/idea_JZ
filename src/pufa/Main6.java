de29cd45dcf1be23eff2f1df42b5871799bc896f
zhangna <907404296@qq.com>
2020-09-13 21:40:39 +0800

    coding with JZ

---
+++ src/pufa/Main6.java
@@ -0,0 +1,52 @@
+package pufa;
+
+public class Main6 {
+    /*
+    题目：输出字符串的最大回文子字符串的长度。
+    思路：中心扩展法
+         长度为奇数的回文串，以字母为中心
+         长度为偶数的回文串，以两个字母之间空隙为中心
+     */
+
+    public static void main(String[] args) {
+        String str = "abbba";
+        System.out.println(getMaxLen(str));
+    }
+
+    public static String getMaxLen(String str){
+        if(str == null || str.length() <= 1)
+            return str;
+
+        int ll = 0;
+        int rr = 0;
+        int res = 1;
+
+        for(int i = 0; i < str.length(); i++){
+            int l = i - 1;
+            int r = i + 1;
+            while (l >= 0 && r < str.length() && str.charAt(l) == str.charAt(r)){
+                int len = (r - l + 1);
+                if(len > res){
+                    res = len;
+                    ll = l;
+                    rr = r;
+                }
+                l--;
+                r++;
+            }
+            l = i;
+            r = i+1;
+            while (l >= 0 && r < str.length() && str.charAt(l) == str.charAt(r)){
+                int len = (r - l + 1);
+                if(len > res){
+                    res = len;
+                    ll = l;
+                    rr = r;
+                }
+                l--;
+                r++;
+            }
+        }
+        return str.substring(ll,rr+1);
+    }
+}
